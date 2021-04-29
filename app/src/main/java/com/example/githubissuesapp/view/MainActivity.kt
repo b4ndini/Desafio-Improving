766f40a354ad325f93652cbee9310c4a892069fa
Luis Maior <lfelipe.maior@gmail.com>
2021-04-28 22:37:57 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/githubissuesapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.githubissuesapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubissuesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
